f93bb77a4808582642eb5af491ee99a0284fd091
chasedashiell <dashchax@gmail.com>
2023-02-23 11:38:46 -0600

    done

--- Main.java
+++ Main.java
@@ -91,7 +91,44 @@ public class Main {
     }
     return solution;
   }
-  
+
+
+
+  public static void printTrans(double[][] nums){
+    double[][] toPrint = new double[nums[0].length][nums.length];
+    for (int row = 0; row < nums.length; row++){
+      for(int col = 0; col < nums[0].length; col++){
+        toPrint[col][row] = nums[row][col];
+      }
+    }
+    print2D(toPrint);
+  }
+
+  public static double[][] withWeightedAverage(double[][]nums, double[] weights) {
+    double[][] solution = new double[nums.length +1][nums[0].length+1];
+    for (int row = 0; row < nums.length; row++){
+      int weightsIndex = 0;
+      for (int col = 0; col < nums[0].length; col++){
+        solution[row][col] = nums[row][col] * weights[weightsIndex];
+        weightsIndex++;
+      }
+    }
+    for (int row = 0; row < solution.length-1; row++){
+      double currentMean = 0;
+      for (int i = 0; i < solution[row].length-1; i++){
+        currentMean+= solution[row][i];
+      }
+      solution[row][solution[row].length-1] = currentMean;
+    }
+    for (int col = 0; col < solution[0].length; col++){
+      double currentMean = 0;
+      for (int row = 0; row < solution.length; row++){
+        currentMean += solution[row][col];
+      }
+      solution[solution.length-1][col] = currentMean / nums.length;
+    }
+    return solution;
+  }
   public static void main(String[] args) {
     //double[][] testSquares = {{1,2,3,9}, {4,5,6,9}, {7,8,9,9},{7,8,9,9}};
     //System.out.println(Arrays.toString(flattenSquare(testSquares)));
@@ -102,7 +139,49 @@ public class Main {
     //double[][] test = {{1,2},{1,2,3,4}};
     //print2D(copySquare(test));
     //print2D(copyRagged(test));
+    /* 
+    double[][] a = new double[][] {
+      {99, 85, 98},
+      {98, 57, 79},
+      {92, 77, 74},
+      {94, 62, 81},
+      {99, 94, 92},
+      {80, 76.5, 67},
+      {76, 58.5, 90.5},
+      {92, 66, 91},
+      {77, 70.5, 66.5},
+      {89, 89.5, 81}
+  };
+    printTrans(a);
+    */
+    double[][] a = new double[][] {
+      {99, 85, 98},
+      {98, 57, 79},
+      {92, 77, 74},
+      {94, 62, 81},
+      {99, 94, 92},
+      {80, 76.5, 67},
+      {76, 58.5, 90.5},
+      {92, 66, 91},
+      {77, 70.5, 66.5},
+      {89, 89.5, 81}
+  };
 
+  // double[][] answer = new double[][] {
+  //   {99, 85, 98, 95},
+  //   {98, 57, 79, },
+  //   {92, 77, 74, },
+  //   {94, 62, 81, },
+  //   {99, 94, 92, },
+  //   {80, 76.5, 67, },
+  //   {76, 58.5, 90.5, },
+  //   {92, 66, 91, },
+  //   {77, 70.5, 66.5, },
+  //   {89, 89.5, 81, }
+  //   {               }
+// };
+  double[] weights = { 0.25, 0.25, 0.50 };
+  print2D(withWeightedAverage(a, weights));
   }
   
 }
